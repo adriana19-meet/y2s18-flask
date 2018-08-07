4bb96bb0e3897ca7b6364de0f471c694567aa5d5
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:11:19 +0300

    conditions , loops

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return "my fav sport is football"
+#def home_page(year=2020):
+    #return render_template (
+    	#"index.html",year= year)
+
+
+def likes_same_sport():
+    players = ["marco asensio", "ronaldo", "iscoo"] 
+    return render_template("index.html",players = players)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
